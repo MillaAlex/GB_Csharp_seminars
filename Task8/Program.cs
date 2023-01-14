c189a6ccc0d2ee5e3ca4782c3ca10f313eb6e759
Lyudmila <liudmila.k1610@gmail.com>
2023-01-14 16:30:09 +0300

    Output of even N from 1 to N

---
+++ Task8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int startnumber = 1;
+
+while (startnumber <= number)
+{
+    if (startnumber % 2 == 0)
+    {
+        Console.Write(startnumber + ", ");
+    }
+    startnumber++;
+}
+Console.Write("\b\b   ");
