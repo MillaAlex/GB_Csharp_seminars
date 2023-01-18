d4fe9ddb749e3847b9aba8da96df8388084cfaae
Lyudmila <liudmila.k1610@gmail.com>
2023-01-18 20:20:18 +0300

    3-digits number -> output 2nd

---
+++ Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string number1 = Convert.ToString(number);
+
+if (number1.Length == 3)
+{
+    int temp = number / 10;
+    int secondnumber = temp % 10;
+    Console.WriteLine("Вторая цифра числа " + number + " -> " + secondnumber);
+}
+else
+{
+    Console.WriteLine("Недопустимое число");
+}
