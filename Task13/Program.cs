16b697a5f8240b6c699d55554f068ddbe8a19ed3
Lyudmila <liudmila.k1610@gmail.com>
2023-01-20 20:06:01 +0300

    third number - second option

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -4,24 +4,51 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.Write("Введите положительное число: ");
+// Вариант 1 (не работает с отрицательными):
+
+// Console.Write("Введите положительное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// string number1 = Convert.ToString(number);
+
+// if (number1.Length == 3)
+// {
+//     int temp = number % 10;
+//     Console.WriteLine("Третья цифра числа " + number + " -> " + temp);
+// }
+// if (number1.Length > 3)
+// {
+//     Console.WriteLine(number + " -> " + number1[2]);  
+// }
+// else
+// {
+//     if (number1.Length < 3)
+//     {
+//         Console.WriteLine("Третьей цифры нет");
+//     }
+// }
+
+// Вариант 2:
+
+Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-string number1 = Convert.ToString(number);
-
-if (number1.Length == 3)
+if (number < 10)
+// т.к. в условии не сказано, что делать с отрицательными и с однозначным числом 
 {
-    int temp = number % 10;
-    Console.WriteLine("Третья цифра числа " + number + " -> " + temp);
+    Console.WriteLine("Недопустимое число");
 }
-if (number1.Length > 3)
+else if (number > 99 & number <= 999)
 {
-    Console.WriteLine(number + " -> " + number1[2]);
+    int thirdnumber = number % 10;
+    Console.WriteLine("Третья цифра числа " + number + " -> " + thirdnumber);
 }
-else
+else if (number > 9 & number <= 99)
 {
-    if (number1.Length < 3)
-    {
-        Console.WriteLine("Третьей цифры нет");
-    }
+    Console.WriteLine(number + " - > третьей цифры нет");
+}
+else if (number > 999)
+{
+    string bignumber = Convert.ToString(number);
+    Console.WriteLine("Третья цифра числа " + number + " -> " + bignumber[2]);
 }
