348c3847e1f1f8fe64babd897d6b0f96ecaa6be0
Lyudmila <liudmila.k1610@gmail.com>
2023-01-27 07:41:31 +0300

    sum of numbers of a number

---
+++ Task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write($"{number} ");
+
+int Sum()
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number /= 10;
+    }
+    return result;
+}
+Console.WriteLine($" -> {(Sum())}");
