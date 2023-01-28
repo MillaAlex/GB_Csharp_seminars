b9de085683a05c74ceb6d3fb0bce009e860bad2d
Lyudmila <liudmila.k1610@gmail.com>
2023-01-28 19:27:17 +0300

    qty of even numbers in array

---
+++ Task34/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int[] array = new int[25];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write("\b\b");
+}
+Console.Write("] -> ");
+
+int counter = 0;
+for (int i = 0; i < array.Length; i++)
+{   
+    if (array[i] % 2 == 0) counter += 1;
+}
+Console.Write(counter);
+
+
+// int QtyOfEven(int[] numbers)
+// {
+//     int counter = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {   
+//         if (array[i] % 2 == 0) counter += 1;
+//     }
+//     return counter;
+// }
+// QtyOfEven(numbers);
