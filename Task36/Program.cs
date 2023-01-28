3ec3671c934ffdc445c93cbb4a314bb264b06acc
Lyudmila <liudmila.k1610@gmail.com>
2023-01-28 19:43:08 +0300

    sum of uneven index elemets in array

---
+++ Task36/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 10);
+    }
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write("\b\b");
+}
+Console.Write("] -> ");
+
+int index = 0;
+int sumUneven = 0;
+while (index < array.Length)
+{
+    if (index % 2 == 1) sumUneven = sumUneven + array[index];
+    index++;
+}
+Console.Write(sumUneven);
