35c7e2c51b0bf0a22a96c64f2be998c546be56a0
Lyudmila <liudmila.k1610@gmail.com>
2023-02-01 21:58:37 +0300

    count pos numbers in array

---
+++ Task41/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] array = new int[3];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Ведите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]} ");
+}
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0) count += 1;
+}
+Console.Write($" -> {count}");
