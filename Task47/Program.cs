051fcafca0cbb374a6b286219da4590904a65f7b
Lyudmila <liudmila.k1610@gmail.com>
2023-02-04 19:31:32 +0300

    double array mxn

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -15,7 +15,7 @@ for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-        array[i, j] = Math.Round((new Random().NextDouble() * 10), 2);
+        array[i, j] = Math.Round(new Random().NextDouble() * new Random().Next(-9, 10), 1);
     }
 }
 
