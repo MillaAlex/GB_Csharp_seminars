8e4e88187e43a637afa5989264f8f655e47d32fb
Lyudmila <liudmila.k1610@gmail.com>
2023-02-10 22:43:45 +0300

    rec from n to 1

---
+++ Task64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая 
+// выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Задайте значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write($"N = {N} -> '{PrintNumbers(N, 1)}'");
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end) return Convert.ToString(end);    
+    return (start + ", " + PrintNumbers(start - 1, end));
+}
